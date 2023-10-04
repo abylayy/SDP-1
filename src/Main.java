36df33e128ccf52fd807aed62352193f61b36f04
Abylay <abylay0505@gmail.com>
2023-10-04 23:07:56 +0600

    singleton pattern added

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
         Worker programmer = new Worker(new Programmer());
         Worker teacher = new Worker(new Teacher());
         Worker footballer = new Worker(new Footballer());
+        Worker footballCoach = new Worker(FootballCoach.getInstance());
 
         programmer.performJob();
         teacher.performJob();
         footballer.performJob();
+        footballCoach.performJob();
 
     }
 }
